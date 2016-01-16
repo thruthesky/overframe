a07d6840313326c6e327526675997836678afb53
JaeHo Song <thruthesky@gmail.com>
2016-01-16 09:46:13 +0900

    work for banner

---
+++ etc/js/ajaxcrud.js
@@ -0,0 +1,6 @@
+$(function(){
+    var $body = $('body');
+    $('[table]').click(function(){
+        alert('show table list through ajax');
+    });
+});
--- template/entity-list/entity-list.php
+++ template/entity-list/entity-list.php
@@ -1,3 +1,6 @@
+<link rel="stylesheet" href="<?php echo url_overframe()?>/etc/css/ajaxcrud.css">
+<script src="<?php echo url_overframe()?>/etc/js/ajaxcrud.js"></script>
+
 <?php
 $db = database();
 $tables = $db->getTables();
@@ -5,7 +8,10 @@ $entities = [];
 foreach ($tables as $table)
 {
     if ( strpos($table, '_entity')  ) {
-        $entities[] = $table;
+        $name = str_replace('_node_entity', '', $table);
+        $name = str_replace('_meta_entity', '', $name);
+        $entities[$name] = $table;
+
     }
     else continue;
 }
@@ -14,5 +20,10 @@ foreach ($tables as $table)
 // @todo CRUD 는 필드별로 할 것. 그래야 깔끔함. 안그러면 처리가 매우 어려워 짐.
 // @todo 새로운 레코드를 생성 할 때에도 그냥 add 버튼만 클릭하면 새로운 행이 생성되도록 한다.
 // @todo 이것이 끝나면 philgo_banner 를 관리 할 수 있는 ACL 테이블을 만든다.
-// acl_node_entity 로 만든다.
-di($entities);
+// @todo acl_node_entity 로 만든다.
+// @todo 주의 복잡한 것은 어렵다. 예를 들어서 파일 업로드를 하는 경우 여러개의 테이블 정보가 필요하다.
+// @todo 따라서 etc/js/ajaxcrud 에 골격을 만들고,각 모듈에서 활용하도록 한다.
+// @todo underscore.js 의 template 을 활용한다.
+foreach ($entities as $name => $table) {
+    echo "<span class='btn btn-primary' table='$table'>$name</span>";
+}
